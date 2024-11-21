7d8152f0158e5c6816d45523e792ea1d8a66fabc
Rahul-0312 <rahulr1@kpmg.com>
2024-11-21 23:05:54 +0530

    Performed TF FMT

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.10.0"
     }
   }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
